ab5260944a306e2e3ea975db414b556ec07673ff
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-17 21:51:51 +0600

    test: add functional tests for POST route to create a new book

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -33,46 +33,84 @@ suite("Functional Tests", function () {
   /*
    * ----[END of EXAMPLE TEST]----
    */
-  // suite('Routing tests', function() {
-  //   suite('POST /api/books with title => create book object/expect book object', function() {
-  //     test('Test POST /api/books with title', function(done) {
-  //       //done();
-  //     });
-  //     test('Test POST /api/books with no title given', function(done) {
-  //       //done();
-  //     });
-  //   });
-  //   suite('GET /api/books => array of books', function(){
-  //     test('Test GET /api/books',  function(done){
-  //       //done();
-  //     });
-  //   });
-  //   suite('GET /api/books/[id] => book object with [id]', function(){
-  //     test('Test GET /api/books/[id] with id not in db',  function(done){
-  //       //done();
-  //     });
-  //     test('Test GET /api/books/[id] with valid id in db',  function(done){
-  //       //done();
-  //     });
-  //   });
-  //   suite('POST /api/books/[id] => add comment/expect book object with id', function(){
-  //     test('Test POST /api/books/[id] with comment', function(done){
-  //       //done();
-  //     });
-  //     test('Test POST /api/books/[id] without comment field', function(done){
-  //       //done();
-  //     });
-  //     test('Test POST /api/books/[id] with comment, id not in db', function(done){
-  //       //done();
-  //     });
-  //   });
-  //   suite('DELETE /api/books/[id] => delete book object id', function() {
-  //     test('Test DELETE /api/books/[id] with valid id in db', function(done){
-  //       //done();
-  //     });
-  //     test('Test DELETE /api/books/[id] with  id not in db', function(done){
-  //       //done();
-  //     });
-  //   });
-  // });
+  suite("Routing tests", function () {
+    suite(
+      "POST /api/books with title => create book object/expect book object",
+      function () {
+        test("Test POST /api/books with title", function (done) {
+          chai
+            .request(server)
+            .keepOpen()
+            .post("/api/books")
+            .send({
+              title: "New Book",
+            })
+            .end(function (err, res) {
+              assert.equal(res.status, 200);
+
+              assert.isObject(res.body, "response should be an object");
+              assert.property(
+                res.body,
+                "title",
+                "Issue object should have a title"
+              );
+
+              assert.property(
+                res.body,
+                "_id",
+                "Issue object should have an id"
+              );
+              done();
+            });
+        });
+
+        test("Test POST /api/books with no title given", function (done) {
+          chai
+            .request(server)
+            .keepOpen()
+            .post("/api/books")
+            .send({})
+            .end(function (err, res) {
+              assert.equal(res.status, 200);
+
+              assert.isString(res.body, "response should be a string");
+              assert.equal(res.body, "missing required field title");
+              done();
+            });
+        });
+      }
+    );
+    //   suite('GET /api/books => array of books', function(){
+    //     test('Test GET /api/books',  function(done){
+    //       //done();
+    //     });
+    //   });
+    //   suite('GET /api/books/[id] => book object with [id]', function(){
+    //     test('Test GET /api/books/[id] with id not in db',  function(done){
+    //       //done();
+    //     });
+    //     test('Test GET /api/books/[id] with valid id in db',  function(done){
+    //       //done();
+    //     });
+    //   });
+    //   suite('POST /api/books/[id] => add comment/expect book object with id', function(){
+    //     test('Test POST /api/books/[id] with comment', function(done){
+    //       //done();
+    //     });
+    //     test('Test POST /api/books/[id] without comment field', function(done){
+    //       //done();
+    //     });
+    //     test('Test POST /api/books/[id] with comment, id not in db', function(done){
+    //       //done();
+    //     });
+    //   });
+    //   suite('DELETE /api/books/[id] => delete book object id', function() {
+    //     test('Test DELETE /api/books/[id] with valid id in db', function(done){
+    //       //done();
+    //     });
+    //     test('Test DELETE /api/books/[id] with  id not in db', function(done){
+    //       //done();
+    //     });
+    //   });
+  });
 });
